2d9142f093cfe0021a2c33c4c2378c94aa047e8c
dahun_27 <hdh011027@gmail.com>
2026-01-13 23:13:28 +0900

    Initial Commit

---
+++ Devvit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Devvit
+//
+//  Created by 하다현 on 1/13/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Devvit/DevvitApp.swift
@@ -0,0 +1,17 @@
+//
+//  DevvitApp.swift
+//  Devvit
+//
+//  Created by 하다현 on 1/13/26.
+//
+
+import SwiftUI
+
+@main
+struct DevvitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
